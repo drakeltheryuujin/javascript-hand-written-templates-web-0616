13f892318b126d4922dcbb740764040a8114a01e
Kellye Greene <kellye@gust.com>
2016-08-03 13:59:55 -0400

    templates

--- test/index-test.js
+++ test/index-test.js
@@ -10,9 +10,5 @@ describe('index', () => {
   jsdom({
     src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
   })
-  
 
-  it('runs', () => {
-    expect(true).to.be.true
-  })
 })
